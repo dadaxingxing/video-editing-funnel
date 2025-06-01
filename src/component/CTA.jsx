72a0d124094bb32128d5ca70a0341913df17b5d0
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-06-01 11:58:28 -0700

    button component work in progress

---
+++ src/component/CTA.jsx
@@ -0,0 +1,11 @@
+import './CTA.css';
+
+function CTA() {
+    return (
+        <div className='button_frame d-flex align-items-center justify-content-center'>
+            <div className='button_text'>temp hewfiujfiosdjfiod</div>
+        </div>
+    )
+};
+
+export default CTA;
