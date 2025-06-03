7c4e990d26e22e9849f296a1be9f330f75746670
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-06-02 20:15:50 -0700

    function called when button pressed

--- src/component/CTA.jsx
+++ src/component/CTA.jsx
@@ -1,12 +1,16 @@
 import './CTA.css';
-
+import { useState } from 'react';
 
 
 function CTA({children, }) {
+    const handleSubmit = () => {
+        console.log('Redirecting user to buttom of funnel!')
+    };
+
     return (
-        <div role='button' className='btn button_frame d-inline-block'>
-            <div className='button_text'>{children}</div>
-        </div>
+            <div role='button' className='btn button_frame d-inline-block' onClick={handleSubmit}>
+                <div className='button_text'>{children}</div>
+            </div>
     )
 };
 
