a05afbc78ec766024de25baa8b4a92d7d7070d0a
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-06-04 14:22:33 -0700

    did some work on the offer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import './App.css';
 import CTA from './component/CTA';
 import Proof from './component/Proof';
 import Cards from './component/Cards';
-
+import Offer from './component/Offer';
 
 function App() {
+  const offerPoints = [
+    'short premium content (10-30s)',
+    '24 hours guaranteed delivery',
+    'thumbnail design',
+    'unlimited revisions',
+    'dedicated video editor'
+  ]
+
 
   return (
     <div className='d-flex flex-column'>
@@ -12,9 +20,18 @@ function App() {
       <Proof>add testimonial here</Proof>
       <Proof>add testimonial here</Proof>
       <Proof>add testimonial here</Proof>
+      
       <Cards 
         title='Review'
         paragraph='Indicate the amount of video needed everyday and fill out ways to contact by filling out the form.'
+      />
+
+      <Offer
+        packages='Standard'
+        description='The ideal plan for daily short-form content needs'
+        price='$35'
+        unit='/video'
+        points={offerPoints}
       />
     </div>
   )
--- src/component/Offer.jsx
+++ src/component/Offer.jsx
@@ -1,1 +1,24 @@
 import './Offer.css';
+import CTA from './CTA';
+
+function Offer({packages, description, price, unit, points}) {
+    return (
+        <div className='offerFrame d-flex flex-column justify-items-center align-items center'>
+            <div className='package'>{packages}</div>
+            <div className='description'>{description}</div>
+            <div className='priceFrame d-flex flex-row'>
+                <div className='price'>{price}</div>
+                <div className='unit'>{unit}</div>
+            </div>
+            <CTA>Get Started</CTA>
+            <div className='bulletPoint'>{points}</div>
+            <ul>
+                {points.map((point, index) => (
+                    <li key={index}>{point}</li>
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+export default Offer;
