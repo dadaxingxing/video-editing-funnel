db2f237777b48e408c07b71756f9b0f55c284a13
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-06-07 07:08:07 -0700

    added navigation tool bar

--- src/App.jsx
+++ src/App.jsx
@@ -15,44 +15,53 @@ function App() {
 
 
   return (
-    <div className='page1 d-flex flex-column justify-content-center align-items-center'>
-      {/* <CTA
-        height='2.8rem'
-        width='7.8rem'
-      >Buy Now</CTA>
-
-      <div className='d-flex flex-row'>
-
-        <Proof>add testimonial here</Proof>
-        <Proof>add testimonial here</Proof>
-        <Proof>add testimonial here</Proof>
+    <>
+      <div className='navBar'>
         
-      </div>
-      <div className='d-flex flex-row'>
 
-        <Cards 
-          title='Review'
-          paragraph='Indicate the amount of video needed everyday and fill out ways to contact by filling out the form.'
-        />
-        <Cards 
-          title='Upload'
-          paragraph='Upload all footage to us along with any requirements to a google drive.'
-        />
-        <Cards 
-          title='Revise'
-          paragraph='You will review our edit until you are satisfied before we move onto the next video.'
-        />
-        
       </div>
 
-      <Offer
-        packages='Standard'
-        description='The ideal plan for daily short-form content needs'
-        price='$35'
-        unit='/video'
-        points={offerPoints}
-      /> */}
-    </div>
+      <div className='page1 d-flex flex-column justify-content-center align-items-center'>
+
+        {/* <CTA
+          height='2.8rem'
+          width='7.8rem'
+        >Buy Now</CTA>
+
+        <div className='d-flex flex-row'>
+
+          <Proof>add testimonial here</Proof>
+          <Proof>add testimonial here</Proof>
+          <Proof>add testimonial here</Proof>
+          
+        </div>
+        <div className='d-flex flex-row'>
+
+          <Cards 
+            title='Review'
+            paragraph='Indicate the amount of video needed everyday and fill out ways to contact by filling out the form.'
+          />
+          <Cards 
+            title='Upload'
+            paragraph='Upload all footage to us along with any requirements to a google drive.'
+          />
+          <Cards 
+            title='Revise'
+            paragraph='You will review our edit until you are satisfied before we move onto the next video.'
+          />
+          
+        </div>
+
+        <Offer
+          packages='Standard'
+          description='The ideal plan for daily short-form content needs'
+          price='$35'
+          unit='/video'
+          points={offerPoints}
+        /> */}
+      </div>
+    
+    </>
 
 
   )
