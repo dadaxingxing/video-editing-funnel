6f3c99ea1407951f408db10b3c2b323773c39d81
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-06-13 15:36:12 -0700

    popup full screen in website, instead of open another window

--- src/component/CTA.jsx
+++ src/component/CTA.jsx
@@ -1,28 +1,61 @@
 import './CTA.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 
 
 function CTA({children, height='2.5rem', width='5.6rem', radius='.25rem', font_size='1.25rem'}) {
     const [showForm, setShowForm] = useState(false);
-    const handleSubmit = () => {
-        window.open("https://tally.so/r/mR6Vo4", "_blank");
-    };
+    const iframeRef = useRef(null);
+    useEffect(() => {
+        if (!showForm && !iframeRef.current) return;
+
+        const queryParams = window.location.search;
+        iframeRef.current.src = `https://tally.so/r/mR6Vo4${queryParams}`;
+    }, [showForm]);
 
     return (
-        <div 
-            role='button' 
-            className='btn button_frame d-flex justify-content-center align-items-center' 
-            onClick={handleSubmit}
-            style={{
-                height,
-                width,
-                borderRadius: radius,
-                fontSize: font_size,
-            }}
-        >
-            <div className='button_text'>{children}</div>
-        </div>
+        <>
+            <div 
+                role='button' 
+                className='btn button_frame d-flex justify-content-center align-items-center' 
+                onClick={() => setShowForm(true)}
+                style={{
+                    height,
+                    width,
+                    borderRadius: radius,
+                    fontSize: font_size,
+                }}
+            >
+                <div className='button_text'>{children}</div>
+            </div>
+
+            {showForm && (
+                <div
+                style={{
+                    position: 'fixed',
+                    top: 0, left: 0, right: 0, bottom: 0,
+                    zIndex: 9999,
+                    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+                }}
+                >
+                    <iframe
+                        ref={iframeRef}
+                        title="Free video editing quota"
+                        frameBorder="0"
+                        marginHeight="0"
+                        marginWidth="0"
+                        style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        border: '0',
+                        }}
+                    />
+                </div>
+            )}
+        </>
     )
 };
 
