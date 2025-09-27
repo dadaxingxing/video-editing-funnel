2a6de07c290fa7bd2fcb51eae5bf259defb36fdf
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-09-27 16:04:35 -0700

    made the signup form on website typeform instead of tally

--- src/component/CTA.jsx
+++ src/component/CTA.jsx
@@ -1,99 +1,45 @@
 import './CTA.css';
-import { useState, useEffect, useRef } from 'react';
+import { PopupButton } from '@typeform/embed-react';
 
 
+function CTA({children, height='2.5rem', width='5.6rem', radius='.25rem', fontSize='1.25rem'}) {
 
-function CTA({children, height='2.5rem', width='5.6rem', radius='.25rem', font_size='1.25rem'}) {
-    const [showForm, setShowForm] = useState(false);
-    const iframeRef = useRef(null);
-    useEffect(() => {
-        if (!showForm || !iframeRef.current) return;
-
-        const queryParams = window.location.search;
-        iframeRef.current.src = `https://tally.so/r/mR6Vo4${queryParams}`;
-    }, [showForm]);
-    
-    useEffect(() => {
-        const handleKeyDown = (e) => {
-            if (e.handleKeyDown === 'escape'){
-                setShowForm(false);
-                if (window.history.state?.modalOpen) {
-                    window.history.back();
-                }
-            }
-        };
-
-        const handlePopState = () => {
-            setShowForm(false);
-        };
-
-        if (showForm) {
-            window.addEventListener('keydown', handleKeyDown);
-            window.addEventListener('popstate', handlePopState);
-            window.history.pushState({ modalOpen: true }, '');
-        }
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-            window.removeEventListener('popstate', handlePopState);
-        };
-    }, [showForm]);
-        
     return (
-        <>
-            <div 
-                role='button' 
-                className='btn button_frame d-flex justify-content-center align-items-center' 
-                onClick={() => setShowForm(true)}
-                style={{
-                    height,
-                    width,
-                    borderRadius: radius,
-                    fontSize: font_size,
-                }}
-            >
-                <div className='button_text'>{children}</div>
-            </div>
+        <PopupButton
+            id="daeQ7EP9"
+            className="btn button_frame d-flex justify-content-center align-items-center"
+            style={{
+                height,
+                width,
+                borderRadius: radius,
+                fontSize,
+            }}
+        >
 
-            {showForm && (
-                <div
-                style={{
-                    position: 'fixed',
-                    top: 0, left: 0, right: 0, bottom: 0,
-                    zIndex: 9999,
-                    backgroundColor: 'rgba(0, 0, 0, 0.75)',
-                }}
-                >
-                    {/* <div 
-                        className='btn button_frame d-flex justify-content-center'
-                        onClick={() => setShowForm(false)}
-                        style={{
-                            height,
-                            width,
-                            borderRadius: radius,
-                            fontSize: font_size,
-                        }}
-                    >
-                        close
-                    </div> */}
-                    <iframe
-                        ref={iframeRef}
-                        title="Free video editing quota"
-                        frameBorder="0"
-                        marginHeight="0"
-                        marginWidth="0"
-                        style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        border: '0',
-                        }}
-                    />
-                </div>
-            )}
-        </>
+        {children}
+        </PopupButton>
+        // <Popup id='01K66NE7EV6A17PHHBMDFHM28H'>
+        //     {({ open }) => (
+        //         <div 
+        //             role='button' 
+        //             className='btn button_frame d-flex justify-content-center align-items-center' 
+        //             onClick={open}
+        //             style={{
+        //                 height,
+        //                 width,
+        //                 borderRadius: radius,
+        //                 fontSize: font_size,
+        //             }}
+        //         >
+        //             <div className='button_text'>{children}</div>
+        //         </div>
+                
+        //     )
+
+        //     }
+        // </Popup>
+
+
     )
 };
 
