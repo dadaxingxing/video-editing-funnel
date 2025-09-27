9bae8ab7f1e03730dee5e3e30fd1909c1acdedfd
dadaxingxing <96450279+dadaxingxing@users.noreply.github.com>
2025-09-27 16:05:15 -0700

    cleaned some stuff

--- src/component/CTA.jsx
+++ src/component/CTA.jsx
@@ -18,26 +18,6 @@ function CTA({children, height='2.5rem', width='5.6rem', radius='.25rem', fontSi
 
         {children}
         </PopupButton>
-        // <Popup id='01K66NE7EV6A17PHHBMDFHM28H'>
-        //     {({ open }) => (
-        //         <div 
-        //             role='button' 
-        //             className='btn button_frame d-flex justify-content-center align-items-center' 
-        //             onClick={open}
-        //             style={{
-        //                 height,
-        //                 width,
-        //                 borderRadius: radius,
-        //                 fontSize: font_size,
-        //             }}
-        //         >
-        //             <div className='button_text'>{children}</div>
-        //         </div>
-                
-        //     )
-
-        //     }
-        // </Popup>
 
 
     )
